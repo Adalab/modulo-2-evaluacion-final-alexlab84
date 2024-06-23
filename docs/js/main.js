62dc5dcf8819bf02d370c6d8c22f1c2a9f27df69
Ale <alexlab84@hotmail.com>
2024-06-23 02:22:41 +0200

    Deployed new version of the page to production

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,4 +1,4 @@
-const a=document.querySelector(".js__charactersCards");let t=[];function c(e){let r="https://via.placeholder.com/210x295/ffffff/555555/?text=Disney";e.imageUrl&&(r=e.imageUrl),a.innerHTML+=`<li class="cards">
-    <img src="${r}" alt="Foto de ${e.name}">
-    <p>${e.name}</p></li>`}function l(){let e="";for(const r of t)e+=c(r);return e}console.log("Holis");console.log("Antes");fetch("https://api.disneyapi.dev/character?pageSize=50").then(e=>e.json()).then(e=>{console.log("Dentro"),t=e.data,l()});console.log("despues");
+const t=document.querySelector(".js__charactersCards");let a=[];function l(e){let c="https://via.placeholder.com/210x295/ffffff/555555/?text=Disney";e.imageUrl&&(c=e.imageUrl),t.innerHTML+=`<li class="js__cards cards">
+    <img class="imgCards" src="${c}" alt="Foto de ${e.name}">
+    <p>${e.name}</p></li>`}function s(){let e="";for(const r of a)e+=l(r);const c=document.querySelectorAll(".js__cards");for(const r of c)r.addEventListener("click",o)}function o(e){console.log("Has hecho click"),console.log(e.currentTarget),e.currentTarget.classList.toggle("favorite")}console.log("Holis");console.log("Antes");fetch("//api.disneyapi.dev/character?pageSize=50").then(e=>e.json()).then(e=>{console.log("Dentro"),a=e.data,s()});console.log("despues");
 //# sourceMappingURL=main.js.map
